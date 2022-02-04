43f0e4ac692c18da6a0cb40a6c90556b9e75fef8
Nevin Foster <nafoste2@asu.edu>
2022-02-04 12:57:38 -0700

    Added dynamic funcitonalities

--- Assets/WeatherAssets/Scripts/DynamicWeatherSystem.cs
+++ Assets/WeatherAssets/Scripts/DynamicWeatherSystem.cs
@@ -5,110 +5,41 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class DynamicWeatherSystem : MonoBehaviour
 {
-
-
     /// <summary>
     /// This lets us know if there is more than one EnvironmentUpdater present at once.
     /// </summary>
     [System.NonSerialized]
     public static DynamicWeatherSystem instance = null;
 
-    [System.Serializable]
-    public struct WeatherStateSettings
-    {
-
-        [Tooltip("Intensity of the sun. (Directional)")]
-        [Range(0, 20)]
-        public float sun_intensity;
-        [Tooltip("Intensity of the skylight. (Environment Lighting)")]
-        [Range(0, 20)]
-        public float skylight_intensity;
-        [Tooltip("Thickness of the fog")]
-        [Range(0, 0.1f)]
-        public float fog_density;
-        [Tooltip("Color of the fog")]
-        public Color fog_color;
-    };
-
-    #region Helper_Functions
-
-    public float GetBaseWeight(float[] weights)
-    {
-        float rtrn = 1;
-        for (int i = 0; i < weights.Length; i++)
-            rtrn -= weights[i];
-        return Mathf.Max(rtrn, 0.0f);
-    }
-
-    public float[] GetWeights()
-    {
-        float[] dynamicWeights = {
-            RainWeight,
-            SnowWeight
-        };
-
-        dynamicWeights =  new float[] {
-            GetBaseWeight(dynamicWeights),
-            RainWeight,
-            SnowWeight
-        };
-
-        float sum = 0;
-        foreach (float f in dynamicWeights)
-            sum += f;
-        for (int i = 0; i < dynamicWeights.Length; i++)
-            dynamicWeights[i] /= sum;
-
-        foreach (float f in dynamicWeights)
-            Debug.Log(f);
-        return dynamicWeights;
-    }
-
-    public WeatherStateSettings GetBlendedState_selective(WeatherStateSettings[] weatherStateSettings, float[] weights)
-    {
-        float sunintensity = 0;
-        float skyintensity = 0;
-        float fogdensity = 0;
-        Color fogcolor = new Color(0, 0, 0);
-
-        for (int i = 0; i < weatherStateSettings.Length; i++)
-        {
-            sunintensity += weatherStateSettings[i].sun_intensity * weights[i];
-            skyintensity += weatherStateSettings[i].skylight_intensity * weights[i];
-            fogdensity += weatherStateSettings[i].fog_density * weights[i];
-            fogcolor += weatherStateSettings[i].fog_color * weights[i];
-        }
-
-        return new WeatherStateSettings() {
-            sun_intensity = sunintensity,
-            skylight_intensity = skyintensity,
-            fog_density = fogdensity,
-            fog_color = fogcolor
-        };
-    }
-
-    #endregion
-
-    public WeatherStateSettings GetBlendedState()
-    {
-        return GetBlendedState_selective(
-            new WeatherStateSettings[] {
-                Sunny, Raining, Snowing
-            },
-            GetWeights()
-        );
-    }
+    #region public_variables
 
     [Header("Weather State")]
     [Tooltip("Daynight cycle. 0.5 is noon, 0 and 1 are midnight, 0.3 is 7am, 0.8 is 7pm")]
-    [Range(0,1)]
-    public float DayNightWeight = 0.35f;
+    [Range(0, 1)]
+    [SerializeField] // Private+Serialized: Only should be updated within this script and in the editor.
+    private float m_DayNightWeight = 0.35f;
     [Tooltip("If and how hard it is raining")]
     [Range(0, 1)]
-    public float RainWeight = 0f;
+    [SerializeField] // Private+Serialized: Only should be updated within this script and in the editor.
+    private float m_RainWeight = 0f;
     [Tooltip("If and how hard it is snowing")]
     [Range(0, 1)]
-    public float SnowWeight = 0f;
+    [SerializeField] // Private+Serialized: Only should be updated within this script and in the editor.
+    private float m_SnowWeight = 0f;
+
+    [Header("Dynamic Weather")]
+    [Tooltip("If this script should dynamically update weather on its own.")]
+    [SerializeField] // Private+Serialized: Only should be updated within this script and in the editor.
+    private bool m_UseDynamicWeather = true;
+    [Tooltip("If not, then the weather will start at the current settings.")]
+    public bool RandomizeWeatherOnAwake = false;
+    [Tooltip("The length (in seconds) one Day/Night cycle")]
+    public float DayLength = 300f;
+    [Tooltip("Used to describe how often the weather should change.")]
+    [Min(0.5f)]
+    public Vector2 WeatherDurationRange = new Vector2(100f, 300f);
+    [Tooltip("Used to describe the seconds that the weather should spend changing (blending) states.")]
+    public float DefaultBlendDuration = 20f;
 
     [Header("State Settings")]
     [Tooltip("What the weather should look like when it is sunny")]
@@ -148,7 +79,7 @@ public class DynamicWeatherSystem : MonoBehaviour
 
     [Header("Raining Settings")]
     [Tooltip("This is the max spawn rate that the rain emitter will use.")]
-    [Range(0,30000)]
+    [Range(0, 30000)]
     public float rain_maxparticles = 20000;
     [Tooltip("The raining effect")]
     public ParticleSystem rainEffect;
@@ -168,6 +99,88 @@ public class DynamicWeatherSystem : MonoBehaviour
     [Tooltip("The lightsource that is the moon")]
     public Light Moon;
 
+    #endregion
+
+    #region Properties
+
+    public bool UseDynamicWeather
+    {
+        get { return m_UseDynamicWeather; }
+        set
+        {
+            if (value)
+            {
+                // Reset the weather counter when activating dynamic weather.
+                weatherCounter = GetRandomWeatherDelay();
+            }
+            m_UseDynamicWeather = value;
+        }
+    }
+
+    public float DayNightWeight
+    {
+        get { return m_DayNightWeight; }
+        set
+        {
+            if (UseDynamicWeather)
+            {
+                Debug.LogWarning("Trying to updated time of day while weather is automatically updated. Consider using SetDayTime(float).");
+            }
+            m_DayNightWeight = value;
+        }
+    }
+
+    public float RainWeight
+    {
+        get { return m_RainWeight; }
+        set
+        {
+            m_RainWeight = value;
+            if (UseDynamicWeather)
+            {
+                Debug.LogWarning("Trying to updated Weather while weather is automatically updated. (Recomended to do this explicitly or use SetWeatherState(float,WeatherState))");
+            }
+        }
+    }
+    public float SnowWeight
+    {
+        get { return m_SnowWeight; }
+        set
+        {
+            m_SnowWeight = value;
+            if (UseDynamicWeather)
+            {
+                Debug.LogWarning("Trying to updated Weather while weather is automatically updated. (Recomended to do this explicitly or use SetWeatherState(float,WeatherState))");
+            }
+        }
+    }
+
+    #endregion
+
+    #region nonsaved_privates
+
+    /// <summary>
+    /// Keeps track of how long the current weather state has been active
+    /// </summary>
+    private float weatherCounter = 0;
+    /// <summary>
+    /// The number of weather states that this script uses.
+    /// </summary>
+    private static readonly int weatherStateCount = System.Enum.GetValues(typeof(WeatherStates)).Length;
+    /// <summary>
+    /// Keeps track of the current weather blend instance.
+    /// </summary>
+    [System.NonSerialized]
+    private IEnumerator[] weatherBlendCoroutines = null;
+
+    /// <summary>
+    /// Keeps track of the current daytime blend instance.
+    /// </summary>
+    private IEnumerator daytimeBlendCoroutine = null;
+
+    #endregion
+
+    #region core_functionalities
     /// <summary>
     /// Awake is called before start. This makes sure that this happens before a frame is rendered.
     /// </summary>
@@ -203,21 +216,36 @@ public class DynamicWeatherSystem : MonoBehaviour
             RenderSettings.ambientMode = UnityEngine.Rendering.AmbientMode.Skybox;
         }*/
         RenderSettings.ambientMode = UnityEngine.Rendering.AmbientMode.Skybox;
+
+        if (weatherBlendCoroutines == null)
+        {
+            weatherBlendCoroutines = new IEnumerator[weatherStateCount];
+            for (int i = 0; i < weatherStateCount; i++)
+                weatherBlendCoroutines[i] = null;
+        }
+
+        if (RandomizeWeatherOnAwake)
+        {
+            weatherCounter = -1f;
+        }
+        else weatherCounter = GetRandomWeatherDelay();
     }
 
     private void UpdateWeather()
     {
         WeatherStateSettings currentState = GetBlendedState();
 
+        float sunset_weight = currentState.sun_intensity * Mathf.InverseLerp(0.8f, 0.7f, DayNightWeight) * Mathf.InverseLerp(0.2f, 0.3f, DayNightWeight);
         if (Sun)
         {
-            Sun.intensity = currentState.sun_intensity;
+            Sun.intensity = currentState.sun_intensity * sunset_weight;
             Sun.transform.rotation = Quaternion.Euler(DayNightWeight * 360 - 90, 0, 0);
         }
 
         if (Moon)
         {
-            Moon.intensity = currentState.sun_intensity * MoonWeightIntensity;
+            Moon.intensity = currentState.sun_intensity * MoonWeightIntensity * (1 - sunset_weight);
+            Moon.transform.rotation = Quaternion.Euler(DayNightWeight * 360 + 90, 0, 0);
         }
 
         if (SkyMaterial && SkyMaterial.HasProperty("_Exposure"))
@@ -246,10 +274,232 @@ public class DynamicWeatherSystem : MonoBehaviour
     {
 #if UNITY_EDITOR
         UpdateEditor();
+
+        if (!UpdateWeatherInEditor) return;
 #endif
+
+        bool updateWeather = false;
+        if (daytimeBlendCoroutine != null)
+        {
+            daytimeBlendCoroutine.MoveNext();
+            updateWeather = true;
+        }
+
+        foreach (IEnumerator c in weatherBlendCoroutines)
+        {
+            if (c != null)
+            {
+                c.MoveNext();
+                updateWeather = true;
+            }
+        }
+
+        if (updateWeather) UpdateWeather();
+        if (!UseDynamicWeather) return;
+
+        weatherCounter -= Time.deltaTime;
+
+        if (weatherCounter <= 0)
+        {
+            weatherCounter = GetRandomWeatherDelay();
+
+            float[] ws = new float[weatherStateCount];
+            for (int i = 0; i < weatherStateCount; i++) ws[i] = 0;
+
+            
+            for (int i = 1; i < weatherStateCount + 1; i++)
+            {
+                float random = Random.value;
+
+                if (random < 0.5f) break;
+
+                int state = Random.Range(0, weatherStateCount);
+                random = Random.value;
+
+                Debug.Log(state + ": " + weatherStateCount);
+                ws[state] = random;
+            }
+
+            for (int i = 0; i < weatherStateCount; i++)
+            {
+                SetWeatherState(ws[i], (WeatherStates)i, DefaultBlendDuration);
+            }
+        }
+
+        m_DayNightWeight += Time.deltaTime / DayLength;
+        while (DayNightWeight >= 1) m_DayNightWeight -= 1;
+    }
+
+    #endregion
+
+    #region public_functions
+    public void SetDayTime(float daytime) { SetDayTime(daytime, DefaultBlendDuration); }
+    public void SetDayTime(float daytime, float blendTime)
+    {
+        daytimeBlendCoroutine = DaytimeBlend_Coroutine(daytime, blendTime);
+    }
+
+    public void SetWeatherState(float value, WeatherStates type) { SetWeatherState(value, type, DefaultBlendDuration); }
+    public void SetWeatherState(float value, WeatherStates type, float blendTime)
+    {
+        int type_i = (int)type;
+
+        if(UseDynamicWeather) weatherCounter = GetRandomWeatherDelay();
+
+        weatherBlendCoroutines[type_i] = WeatherBlend_Coroutine(value, type_i, blendTime);
+    }
+
+    #endregion
+
+    #region Helper_Functions
+
+    /**********************************************************/
+    /*                      Coroutines                        */
+
+    private IEnumerator WeatherBlend_Coroutine(float target, int type_i, float blendTime)
+    {
+        float original = BaseWeights[type_i];
+        float counter = 0;
+        do
+        {
+            counter += Time.deltaTime;
+            SetBaseWeight(Mathf.Lerp(original, target, counter / blendTime), (WeatherStates) type_i);
+            
+            yield return null;
+        } while (counter < blendTime);
+
+        weatherBlendCoroutines[type_i] = null;
+    }
+
+    private IEnumerator DaytimeBlend_Coroutine(float daytime, float blendTime)
+    {
+        float originalTime = DayNightWeight;
+        float counter = 0;
+        do
+        {
+            counter += Time.deltaTime;
+            m_DayNightWeight = Mathf.Lerp(originalTime, daytime, counter / blendTime);
+            
+            yield return null;
+        } while (counter < blendTime);
+
+        daytimeBlendCoroutine = null;
+    }
+
+    /**********************************************************/
+
+    public float GetDayWeight(float[] weights)
+    {
+        float rtrn = 1;
+        for (int i = 0; i < weights.Length; i++)
+            rtrn -= weights[i];
+        return Mathf.Max(rtrn, 0.0f);
+    }
+
+    private float[] BaseWeights { get { return new float[] {
+        RainWeight,
+        SnowWeight
+    }; } }
+
+    private void SetBaseWeight(float value, WeatherStates type)
+    {
+        switch(type)
+        {
+            case WeatherStates.Rain:
+                m_RainWeight = value;
+                return;
+            case WeatherStates.Snow:
+                m_SnowWeight = value;
+                return;
+        }
+    }
+
+    private float[] GetAllWeights()
+    {
+        float[] base_W = BaseWeights;
+        float[] dynamicWeights = new float[weatherStateCount + 1];
+        dynamicWeights[0] = GetDayWeight(base_W);
+        for (int i = 0; i < weatherStateCount; i++)
+            dynamicWeights[i + 1] = base_W[i];
+
+        float sum = 0;
+        foreach (float f in dynamicWeights)
+            sum += f;
+        for (int i = 0; i < dynamicWeights.Length; i++)
+            dynamicWeights[i] /= sum;
+
+        return dynamicWeights;
+    }
+
+    private WeatherStateSettings GetBlendedState_selective(WeatherStateSettings[] weatherStateSettings, float[] weights)
+    {
+        float sunintensity = 0;
+        float skyintensity = 0;
+        float fogdensity = 0;
+        Color fogcolor = new Color(0, 0, 0);
+
+        for (int i = 0; i < weatherStateSettings.Length; i++)
+        {
+            sunintensity += weatherStateSettings[i].sun_intensity * weights[i];
+            skyintensity += weatherStateSettings[i].skylight_intensity * weights[i];
+            fogdensity += weatherStateSettings[i].fog_density * weights[i];
+            fogcolor += weatherStateSettings[i].fog_color * weights[i];
+        }
+
+        return new WeatherStateSettings()
+        {
+            sun_intensity = sunintensity,
+            skylight_intensity = skyintensity,
+            fog_density = fogdensity,
+            fog_color = fogcolor
+        };
+    }
+
+    private WeatherStateSettings GetBlendedState()
+    {
+        return GetBlendedState_selective(
+            new WeatherStateSettings[] {
+                Sunny, Raining, Snowing
+            },
+            GetAllWeights()
+        );
+    }
+
+    private float GetRandomWeatherDelay() { return Mathf.Lerp(WeatherDurationRange.x, WeatherDurationRange.y, Random.value); }
+
+#endregion
+
+    /// <summary>
+    /// This struct helps manage the data for general weather states (Sunny, raining, snowing...).
+    /// </summary>
+    [System.Serializable] // Can be saved/edited in the editor
+    public struct WeatherStateSettings
+    {
+
+        [Tooltip("Intensity of the sun. (Directional)")]
+        [Range(0, 20)]
+        public float sun_intensity;
+        [Tooltip("Intensity of the skylight. (Environment Lighting)")]
+        [Range(0, 20)]
+        public float skylight_intensity;
+        [Tooltip("Thickness of the fog")]
+        [Range(0, 0.1f)]
+        public float fog_density;
+        [Tooltip("Color of the fog")]
+        public Color fog_color;
+    };
+
+    /// <summary>
+    /// Identifies the specific weather settings that the weather system has
+    /// </summary>
+    public enum WeatherStates
+    {
+        Rain = 0,
+        Snow = 1,
     }
 
 
+    #region editor
 #if UNITY_EDITOR
 
     [Header("Editor")]
@@ -261,26 +511,24 @@ public class DynamicWeatherSystem : MonoBehaviour
         if (SimulateEffectsInEditor)
         {
             if (rainEffect)
-            {
                 rainEffect.Simulate(Time.deltaTime);
-            }
 
             if (snowEffect)
-            {
                 snowEffect.Simulate(Time.deltaTime);
-            }
         }
         else
         {
             if (rainEffect)
-            {
                 rainEffect.Clear();
-            }
 
             if (snowEffect)
-            {
                 snowEffect.Clear();
-            }
+        }
+
+        if (!UpdateWeatherInEditor)
+        {
+            for (int i = 0; i < weatherBlendCoroutines.Length; i++)
+                weatherBlendCoroutines[i] = null;
         }
     }
 
@@ -303,4 +551,5 @@ public class DynamicWeatherSystem : MonoBehaviour
         UpdateWeather();
     }
 #endif
+#endregion
 }
